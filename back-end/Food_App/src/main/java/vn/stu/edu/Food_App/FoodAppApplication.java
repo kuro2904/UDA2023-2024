7b92b670d2550e64353e9788543ff5dd9ec3b805
kuro2904 <trung2001sgp@gmail.com>
2024-02-19 21:17:28 +0700

    Init project

---
+++ back-end/Food_App/src/main/java/vn/stu/edu/Food_App/FoodAppApplication.java
@@ -0,0 +1,13 @@
+package vn.stu.edu.Food_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppApplication.class, args);
+	}
+
+}
