40fc5c9d1800a09dda680b69a1e8e8fc425710c0
Ton Duc Thang <tncthng347@gmail.com>
2024-02-24 01:11:33 +0700

    Add return button

--- android/food_app/lib/screens/login.dart
+++ android/food_app/lib/screens/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_app/theme/theme.dart';
 import 'package:icons_plus/icons_plus.dart';
 
-// for testing rmove later
+// for testing remove later
 // void main() => runApp(const MyApp());
 //
 // class MyApp extends StatelessWidget {
@@ -21,7 +21,7 @@ import 'package:icons_plus/icons_plus.dart';
 //
 
 class LoginPage extends StatefulWidget  {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() {
@@ -36,134 +36,148 @@ class LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(40),
-      child: Column(
-        children: [
-          Text(
-            "Login",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.blue,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-            child: TextField(
-              controller: nameController,
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          children: [
+            const Text(
+              "Login",
               style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                labelText: "UserName:",
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 15,
-                )
+                fontSize: 30,
+                color: Colors.blue,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-            child: TextField(
-              obscureText: true,
-              controller: passwordController,
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                  labelText: "Password:",
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,
-                  )
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: TextField(
+                controller: nameController,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                decoration: const InputDecoration(
+                    labelText: "UserName:",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    )
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Checkbox(
-                    value: rememberMe,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        rememberMe = value!;
-                      });
-                    },
-                    activeColor: lightColorScheme.primary,
-                  ),
-                  Text(
-                    "Remember me",
-                    style: TextStyle(
-                      color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+                decoration: const InputDecoration(
+                    labelText: "Password:",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    )
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Checkbox(
+                      value: rememberMe,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          rememberMe = value!;
+                        });
+                      },
+                      activeColor: lightColorScheme.primary,
                     ),
+                    const Text(
+                      "Remember me",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                  onTap: passwordForgot,
+                  child: const Text(
+                    "Forget Password?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: login,
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                    fontSize: 18
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: Divider()),
+                  Text("Or"),
+                  Expanded(child: Divider()),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: googleLogin,
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                    ),
+                    child: Brand(Brands.google),
                   ),
                 ],
               ),
-              GestureDetector(
-                onTap: PasswordForgot,
-                child: Text(
-                  "Forget Password?",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+            ),
+            const Spacer(), // Spacing
+            Center(
+              child: ElevatedButton( // Nút quay lại
+                onPressed: () {
+                  Navigator.pop(context); // Quay lại screen trước đó
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
                 ),
+                child: const Icon(Icons.close),
               ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: Login,
-            child: Text(
-              "Login",
-              style: TextStyle(
-                fontSize: 18
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Divider()),
-                Text("Or"),
-                Expanded(child: Divider()),
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: GoogleLogin,
-                  child: Brand(Brands.google),
-                  style: ElevatedButton.styleFrom(
-                    shape: CircleBorder(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
 
-  GoogleLogin() {
+  googleLogin() {
     // TODO: google login
   }
 
-  Login() {
+  login() {
     // TODO: Login logic
   }
 
-  PasswordForgot() {
+  passwordForgot() {
     // TODO: password forgot
   }
 
