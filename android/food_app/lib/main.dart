b3126bca6691f3e542f702b5875216f036d73375
kuro2904 <trung2001sgp@gmail.com>
2024-03-05 10:15:49 +0700

    Delete authentication_generate_token.dart Add Authentication Singleton in client_state.dart Add Insert functions for Category and Product Update backend_config.dart ( Login and register String) Add Login Page for Admin

--- android/food_app/lib/main.dart
+++ android/food_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:food_app/screens/admin/login_page.dart';
-import 'package:food_app/screens/admin/main_page.dart';
 import 'package:food_app/screens/android/home.dart';
 
 
